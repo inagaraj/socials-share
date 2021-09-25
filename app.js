e270aa8ab95cceb13f73df7c7c39af4fc19aa106
Nagaraj <nagaraj2794@rediffmail.com>
2021-09-25 16:32:57 +0530

    updated download link

--- app.js
+++ app.js
@@ -36,6 +36,53 @@ jQuery(document).on("click", "#share-telegram-icon", function() {
     var href = "https://t.me/share/url?url="+quotes_text;
     window.open(encodeURI(href), "_blank");
 });
+jQuery(".wp-block-image").each(function() {
+
+    var imageurl = jQuery(this).find("img").attr("src");
+    var download_html =`<div class="download-link">
+                            <a id="download_nopurchased" class="click_download icon-download" href="${imageurl}"  download="${imageurl}" data-url="${imageurl}"><i class="fas fa-download"></i> Download</a>
+                        </div>`;
+    jQuery(this).append(download_html);
+});
+jQuery(".click_download").click(function() {
+    var file = jQuery(this).data("url");
+    var file_name = getFileName(file);
+    forceDownload(file,file_name);
+    //window.location.href="'.$plugin_url.'?file=file;
+    // setTimeout(function(){ location.reload(); }, 2000);
+    // var formData = new FormData();
+    // formData.append("file", file);
+    // jQuery.ajax({
+    //     url: "./wp-content/plugins/custom-socials-share/download.php",
+    //     contentType: false,
+    //     processData: false,
+    //     data: formData,
+    //     type: "POST",
+    //     success: function (data) {
+    //         console.log(data);
+        
+    //     }
+    // });
+});
+function getFileName(str) {
+    return str.substring(str.lastIndexOf('/') + 1)
+}
+function forceDownload(url, fileName){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+}
 
 new ClipboardJS('.copyit');  
 
@@ -44,3 +91,4 @@ jQuery(document).on("click", ".copyit", function() {
     jQuery(this).find('span').fadeOut(1000);
 
 });
+
